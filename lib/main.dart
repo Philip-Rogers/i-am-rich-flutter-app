a3f8b651c16649dc2f23d92975f86323fca6705c
spaschotic <87228774+spaschotic@users.noreply.github.com>
2022-02-13 00:26:50 +1000

    init

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Text('I Am Rich'),
+          title: Text('I AM RICH'),
           backgroundColor: Colors.blueGrey[900],
         ),
         body: Center(
